07e4816f6b25631187d84e2e28a4d40177f95836
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-12-05 16:01:46 +0900

    updating dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import colors from "./assets/colors/index.tsx";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// Example usage of colors
+
 const rootElement = document.getElementById("root")!;
 rootElement.style.setProperty("--primary-color", colors().primary);
 rootElement.style.setProperty("--secondary-color", colors().secondary);
 rootElement.style.setProperty("--tertiary-color", colors().tertiary);
 
-createRoot(rootElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
