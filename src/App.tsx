b72593f5e25f252fc1c45dcd5b1d932219f9e19e
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-12-06 00:51:34 +0900

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import "./assets/fonts/font-roboto.css";
 import Header from "./components/header/header";
 import Body from "./components/home/body";
