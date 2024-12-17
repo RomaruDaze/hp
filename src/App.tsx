a367cd37a87fa524979bb83a76cb5dcab7a4b6bc
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-12-17 13:45:36 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./assets/fonts/font-roboto.css";
 import Header from "./components/header/header";
 import Body from "./components/home/body";
