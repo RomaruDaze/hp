0856c30d2b2b7518b36a4ac16887d67addc17b3a
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-12-17 14:45:55 +0900

    commit

--- src/components/home/home/home.tsx
+++ src/components/home/home/home.tsx
@@ -13,8 +13,35 @@ function Home() {
     return () => clearTimeout(timer);
   }, []);
 
+  useEffect(() => {
+    const grid = document.querySelector(".grid");
+    const cells = grid?.querySelectorAll(".grid-cell");
+
+    const handleMouseMove = (e: MouseEvent) => {
+      const { clientX, clientY } = e;
+      cells?.forEach((cell) => {
+        const rect = (cell as HTMLElement).getBoundingClientRect();
+        const distance = Math.hypot(clientX - rect.x, clientY - rect.y);
+        const maxDistance = 120;
+        const opacity = Math.max(0, 1 - distance / maxDistance);
+        (
+          cell as HTMLElement
+        ).style.backgroundColor = `rgba(255,215,0, ${opacity})`;
+        (cell as HTMLElement).style.borderColor = `rgba(0,0,0, ${opacity})`;
+      });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
     <div className="home-container">
+      <div className="grid">
+        {Array.from({ length: 900 }).map((_, index) => (
+          <div key={index} className="grid-cell"></div>
+        ))}
+      </div>
       <div
         className="title-container"
         style={{ opacity: 0, animation: "fadeIn 1s forwards" }}
